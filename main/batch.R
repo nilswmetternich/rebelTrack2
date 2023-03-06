ac5436b9d843e47d85ef7d489b532e92c8542320
nilswmetternich <nilsmetternich@gmail.com>
2023-03-06 14:59:08 +0000

    update

--- main/batch.R
+++ main/batch.R
@@ -23,5 +23,6 @@ source('~/Documents/git/rebelTrack2/main/rebel_grids_ged.R', chdir = TRUE)
 source('~/Documents/git/rebelTrack2/main/rebel_transnational_ged.R', chdir = TRUE)
 
 source('~/Documents/git/rebelTrack2/main/merge.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main/lag.R', chdir = TRUE)
 
 source('~/Documents/git/rebelTrack2/main/rebel_adjacency_ged.R', chdir = TRUE)
