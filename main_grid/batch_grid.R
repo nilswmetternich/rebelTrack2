f7a940e8fceda68fd216e6750652db1182cf9c04
nilswmetternich <nilsmetternich@gmail.com>
2023-03-22 15:46:39 +0000

    updated lag structure

--- main_grid/batch_grid.R
+++ main_grid/batch_grid.R
@@ -13,23 +13,23 @@ library(igraph)
 library(nnet)
 library(VGAM)
 
-source('~/Documents/git/rebelTrack2/main/actors_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/actors_grid.R', chdir = TRUE)
 
-source('~/Documents/git/rebelTrack2/main/ingester_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/ingester_grid.R', chdir = TRUE)
 
-source('~/Documents/git/rebelTrack2/main/rebel_best_ged_grid.R', chdir = TRUE)
-source('~/Documents/git/rebelTrack2/main/rebel_days_ged_grid.R', chdir = TRUE)
-source('~/Documents/git/rebelTrack2/main/rebel_deathsA_ged_grid.R', chdir = TRUE)
-source('~/Documents/git/rebelTrack2/main/rebel_deathsB_ged_grid.R', chdir = TRUE)
-source('~/Documents/git/rebelTrack2/main/rebel_distance_ged_grid.R', chdir = TRUE)
-source('~/Documents/git/rebelTrack2/main/rebel_events_ged_grid.R', chdir = TRUE)
-source('~/Documents/git/rebelTrack2/main/rebel_grids_ged_grid.R', chdir = TRUE)
-source('~/Documents/git/rebelTrack2/main/rebel_transnational_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_best_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_days_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_deathsA_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_deathsB_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_distance_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_events_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_grids_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_transnational_ged_grid.R', chdir = TRUE)
 
-source('~/Documents/git/rebelTrack2/main/merge_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/merge_grid.R', chdir = TRUE)
 
-source('~/Documents/git/rebelTrack2/main/rebel_adjacency_ged_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/rebel_adjacency_ged_grid.R', chdir = TRUE)
 
-source('~/Documents/git/rebelTrack2/main/slag_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/slag_grid.R', chdir = TRUE)
 
-source('~/Documents/git/rebelTrack2/main/lag_grid.R', chdir = TRUE)
+source('~/Documents/git/rebelTrack2/main_grid/lag_grid.R', chdir = TRUE)
